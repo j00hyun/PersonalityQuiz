8ca77d65af4697c0ccad0f9b918267d0ffcaa374
j00hyun <loove1997@naver.com>
2022-04-25 20:18:03 +0900

    design:  기본 UI 구성

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Park JooHyun on 2022/04/25.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor:UIColor? {
+        set {
+            layer.borderColor = newValue!.cgColor
+        }
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+    }
+    
+    @IBInspectable var borderWidth:CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius:CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
